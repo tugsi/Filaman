ebb08a7a665b5ba0d8cb9395c4006f6e5d7ced4c
Manuel Weiser <manuel.weiser@me.com>
2025-02-20 18:37:51 +0100

    ci: streamline release workflows by removing unnecessary binary uploads and adding SPIFFS formatting

--- scripts/pre_spiffs.py
+++ scripts/pre_spiffs.py
@@ -5,3 +5,17 @@ exec(open("./scripts/pre_build.py").read())
 
 # Bind to SPIFFS build
 env.AddPreAction("buildfs", replace_version)
+
+import os
+import shutil
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+
+# Format SPIFFS partition before uploading new files
+spiffs_dir = os.path.join(env.subst("$BUILD_DIR"), "spiffs")
+if os.path.exists(spiffs_dir):
+    shutil.rmtree(spiffs_dir)
+os.makedirs(spiffs_dir)
+
+print("SPIFFS partition formatted.")
